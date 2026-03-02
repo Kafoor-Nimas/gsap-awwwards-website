6128f9f57c410ab9a2c26c1179cfc800c68fe0e7
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-02 11:48:09 +0530

    first commit

---
+++ src/sections/FooterSection.jsx
@@ -0,0 +1,94 @@
+import { useMediaQuery } from "react-responsive";
+
+const FooterSection = () => {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 768px)",
+  });
+  return (
+    <section className="footer-section">
+      <img
+        src="public/images/footer-dip.png"
+        alt=""
+        className="w-full object-cover -translate-y-1"
+      />
+      <div className="2xl:h-[110dvh] relative md:pt-[20vh] pt-[10vh]">
+        <div className="overflow-hidden z-10">
+          <h1 className="general-title text-center text-milk py-5">
+            #CHUGRESPONSIBLY
+          </h1>
+        </div>
+
+        {isMobile ? (
+          <img
+            src="public/images/footer-drink.png"
+            alt=""
+            className="absolute top-0 object-contain"
+          />
+        ) : (
+          <video
+            src="public/videos/splash.mp4"
+            autoPlay
+            playsInline
+            muted
+            className="absolute top-0 object-contain mix-blend-lighten"
+          ></video>
+        )}
+
+        <div className="flex-center gap-5 relative z-10 md:mt-20 mt-5">
+          <div className="social-btn">
+            <img src="public/images/yt.svg" alt="" />
+          </div>
+          <div className="social-btn">
+            <img src="public/images/insta.svg" alt="" />
+          </div>
+          <div className="social-btn">
+            <img src="public/images/tiktok.svg" alt="" />
+          </div>
+        </div>
+        <div className="mt-40 md:px-10 px-5 flex gap-10 md:flex-row flex-col justify-between text-milk font-paragraph md:text-lg font-medium">
+          <div className="flex items-center md:gap-16 gap-5">
+            <div>
+              <p>SPYLT Flavors</p>
+            </div>
+            <div>
+              <p>Chug Club</p>
+              <p>Student Marketing</p>
+              <p>Dairy dealers</p>
+            </div>
+            <div>
+              <p>Company</p>
+              <p>Contact</p>
+              <p>Tasty Talk</p>
+            </div>
+          </div>
+          <div className="md:max-w-lg">
+            <p>
+              Get Exclusive Early Access and Stay Informed About Product
+              Updates, Events, and More!
+            </p>
+          </div>
+          <div className="flex justify-between items-center border-b border-[#D9D9D9] py-5 md:mt-10 ">
+            {/* The input field and arrow icon for newsletter signup. */}{" "}
+            {/* A
+          border at the bottom for a clean, modern look. */}
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-full placeholder:font-sans placeholder:text-[#999999]"
+            />
+            <img src="public/images/arrow.svg" alt="arrow" />
+          </div>
+        </div>
+        <div className="copyright-box xl:-m-4">
+          <p>Copyright © 2025 Spylt - All rights Reserved</p>
+          <div className="flex items-center gap-7 ">
+            <p>Privacy Policy</p>
+            <p>Terms of Services</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FooterSection;
